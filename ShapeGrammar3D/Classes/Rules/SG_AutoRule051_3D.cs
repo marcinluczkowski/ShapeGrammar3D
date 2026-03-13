dab9d2f91cb0621e3905a156100e3e49ec176353
bunjii <bunji.izumi@gmail.com>
2026-03-13 12:19:53 +0100

    fixing rule 51

--- ShapeGrammar3D/Classes/Rules/SG_AutoRule051_3D.cs
+++ ShapeGrammar3D/Classes/Rules/SG_AutoRule051_3D.cs
@@ -1,4 +1,4 @@
-    using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -121,63 +121,63 @@ namespace ShapeGrammar3D.Classes.Rules
                     flg_start_or_end = true;
                 }
 
-                // Order along initial curve by parameter t (ClosestPoint of strut base on curve) — not distance from start
-                var targetStudsWithT = new List<(double t, SG_Node tip)>();
-                double t0 = 0;
-                iniCrv.ClosestPoint(stud0.Nodes[0].Pt, out t0);
-
+                var targetStudTipNodes = new List<SG_Node>();
                 for (int j = 0; j < studElements.Count; j++)
                 {
-                    if (j == i) continue;
+                    if (j == i) continue; // skip the current stud to avoid zero-length lines
 
                     var stud = (SG_Elem1D)studElements[j];
-                    // Parameter t from the strut end that lies on the curve (bottom); tip = other end
-                    double t0n, t1n;
-                    iniCrv.ClosestPoint(stud.Nodes[0].Pt, out t0n);
-                    iniCrv.ClosestPoint(stud.Nodes[1].Pt, out t1n);
-                    Point3d on0 = iniCrv.PointAt(t0n), on1 = iniCrv.PointAt(t1n);
-                    double d0 = stud.Nodes[0].Pt.DistanceTo(on0), d1 = stud.Nodes[1].Pt.DistanceTo(on1);
-                    if (d0 > UT.PRES && d1 > UT.PRES) continue; // strut not attached to this curve
-                    double t = d0 <= d1 ? t0n : t1n;
-                    SG_Node tip = d0 <= d1 ? stud.Nodes[1] : stud.Nodes[0];
-                    targetStudsWithT.Add((t, tip));
+
+                    // if (stud.Crv.PointAtStart.CompareTo(iniCrv.PointAtStart) == 0) 
+                    double t;
+                    iniCrv.ClosestPoint(stud.Crv.PointAtStart, out t);
+
+                    if (iniCrv.PointAt(t).CompareTo(stud.Crv.PointAtStart) == 0)
+                    {
+                        targetStudTipNodes.Add(stud.Nodes[1]);
+                    }
+
+                    else if (iniCrv.PointAt(t).CompareTo(stud.Crv.PointAtEnd) == 0)
+                    {
+                        targetStudTipNodes.Add(stud.Nodes[0]);
+                    }
+
+                    else
+                    {
+
+                    }
+
                 }
 
-                // Previous = immediate left along curve (max t < t0), next = immediate right (min t > t0)
-                var previousEntry = targetStudsWithT.Where(p => p.t < t0).OrderByDescending(p => p.t).FirstOrDefault();
-                var nextEntry = targetStudsWithT.Where(p => p.t > t0).OrderBy(p => p.t).FirstOrDefault();
-                var closestStuds = new List<SG_Node>();
-                if (previousEntry.tip != null) closestStuds.Add(previousEntry.tip);
-                if (nextEntry.tip != null) closestStuds.Add(nextEntry.tip);
+                var closestStuds = targetStudTipNodes.OrderBy(n => n.Pt.DistanceTo(stud0.Nodes[1].Pt)).ToList();
 
 
                 SG_Elem1D newElem0 = new SG_Elem1D();
                 SG_Elem1D newElem1 = new SG_Elem1D();
 
-                if (closestStuds.Count >= 1)
+                if (closestStuds.Count > 2)
                 {
                     var ln0 = new Line(stud0.Nodes[1].Pt, closestStuds[0].Pt);
                     var ln1 = new Line(stud0.Nodes[1].Pt, closestStuds[1].Pt);
 
                     bool ln0Valid = ln0.IsValid && ln0.Length > UT.PRES;
+                    bool ln1Valid = ln1.IsValid && ln1.Length > UT.PRES;
+
                     if (ln0Valid)
                         newElem0 = new SG_Elem1D(ln0, -999, "3DAR5", def_crosec) { Autorule = UT.RULE051_MARKER };
-
-                    bool ln1Valid = false;
-                    if (closestStuds.Count >= 2)
-                    {
-                        var ln1 = new Line(myTip, closestStuds[1].Pt);
-                        ln1Valid = ln1.IsValid && ln1.Length > UT.PRES;
-                        if (ln1Valid)
-                            newElem1 = new SG_Elem1D(ln1, -999, "3DAR5", def_crosec) { Autorule = UT.RULE051_MARKER };
-                    }
+                    if (ln1Valid)
+                        newElem1 = new SG_Elem1D(ln1, -999, "3DAR5", def_crosec) { Autorule = UT.RULE051_MARKER };
 
                     if (!flg_start_or_end)
                     {
                         if (optionNumber == 1 && ln0Valid)
+                        {
                             ss_ref.AddNewElement(newElem0);
+                        }
                         else if (optionNumber == 2 && ln1Valid)
+                        {
                             ss_ref.AddNewElement(newElem1);
+                        }
                         else if (optionNumber == 3)
                         {
                             if (ln0Valid) ss_ref.AddNewElement(newElem0);
